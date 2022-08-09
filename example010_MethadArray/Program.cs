e8d44af836a4a403d4e10a054e31eb049ab653e1
Melnikov <artem.melnikov.ara@gmail.com>
2022-08-09 16:35:31 +0300

    Example010 - Method Array

--- example010_MethadArray/Program.cs
+++ example010_MethadArray/Program.cs
@@ -2,9 +2,9 @@
 {
     int length = collection.Length;
     int index = 0;
-    while (index<length)
+    while (index < length)
     {
-        collection[index]=new Random().Next(1,10);
+        collection[index] = new Random().Next(1, 10);
     }
 }
 
@@ -19,7 +19,28 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine();
